0f1ec309af5ac5cc28866ecc1fbcf3ccbe6fcc60
jeshwanthshivasai <havingthesensesofaspider@gmail.com>
2024-02-17 15:35:49 +0530

    change

--- kondachiluva.py
+++ kondachiluva.py
@@ -1,25 +1,53 @@
-# #Running Python in the terminal session
-# print("Hello Python Interpreter!")
-# ----------------------------------------------------------------
-# #Variables
-# message = "Hello Python World!"
-# print(message)
+#Running Python in the terminal session
+print("Hello Python Interpreter!")
+--------------------------------------------------------------
+#Variables
+message = "Hello Python World!"
+print(message)
 
-# message = "Hello Python Interpreter!"
-# print(message)
+message = "Hello Python Interpreter!"
+print(message)
 
-# message = "Hello Python Crash Course World!"
-# print(message)
-# ----------------------------------------------------------------
-# #Variables are labels
-# #1 Simple Message
-# message = "Hello Python"
-# print(message)
+message = "Hello Python Crash Course World!"
+print(message)
+--------------------------------------------------------------
+#Variables are labels
+#1 Simple Message
+message = "Hello Python"
+print(message)
 
-# #2 Simple Messages
-# message = "Hello Python Program"
-# print(message)
-# message = "Hello Python Programming"
-# print(message)
-# ----------------------------------------------------------------
-# #Strings
+#2 Simple Messages
+message = "Hello Python Program"
+print(message)
+message = "Hello Python Programming"
+print(message)
+--------------------------------------------------------------
+#Strings
+#title method
+name = "jeshwanth shiva sai"
+print(name.title())
+#upper method
+name = "jeshwanth shiva sai"
+print(name.upper())
+#lower method
+name = "jeshwanth shiva sai"
+print(name.lower())
+----------------------------------------------------------------
+#Variable in string
+firstname = "Jeshwanth"
+middlename = "Shiva"
+lastname = "Sai"
+fullname = f"{firstname} {middlename} {lastname}"
+print(fullname)
+
+firstname = "schola"
+lastname = "eburouh"
+fullname = f"{firstname} {lastname}"
+print(f"Hello, {fullname.title()}!")
+
+firstname = "jeshwanth"
+lastname = "shiva sai"
+fullname = f"{firstname} {lastname}"
+message = f"Hello, {fullname.title()}!"
+print(message)
+----------------------------------------------------------------
