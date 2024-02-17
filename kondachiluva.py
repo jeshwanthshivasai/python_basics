0b78c8254ce911bb978b528a9d0c4f7f8105b303
jeshwanthshivasai <havingthesensesofaspider@gmail.com>
2024-02-17 07:35:30 +0530

    change

--- kondachiluva.py
+++ kondachiluva.py
@@ -1,6 +1,25 @@
-message = "Hello Python Interpreter!"
-print(message)
+# #Running Python in the terminal session
+# print("Hello Python Interpreter!")
+# ----------------------------------------------------------------
+# #Variables
+# message = "Hello Python World!"
+# print(message)
 
-message = "Hello Python Crash Course World!"
-print(message)
+# message = "Hello Python Interpreter!"
+# print(message)
 
+# message = "Hello Python Crash Course World!"
+# print(message)
+# ----------------------------------------------------------------
+# #Variables are labels
+# #1 Simple Message
+# message = "Hello Python"
+# print(message)
+
+# #2 Simple Messages
+# message = "Hello Python Program"
+# print(message)
+# message = "Hello Python Programming"
+# print(message)
+# ----------------------------------------------------------------
+# #Strings
