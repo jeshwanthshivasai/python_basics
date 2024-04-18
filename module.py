1cbca0fee73c455cc0709a01eb9080c5d3b70fc4
jeshwanthshivasai <havingthesensesofaspider@gmail.com>
2024-04-18 11:08:46 +0530

    change

--- module.py
+++ module.py
@@ -2,6 +2,6 @@
 # kondachiluva.pizza(16, 'pepperoni')
 # kondachiluva.pizza(12, 'mushrooms', 'olives', 'jalepenos')
 
-from kondachiluva import pizza
-pizza(16, 'pepperoni')
-pizza(12, 'mushrooms', 'olives', 'jalepenos')
+# from kondachiluva import pizza
+# pizza(16, 'pepperoni')
+# pizza(12, 'mushrooms', 'olives', 'jalepenos')
