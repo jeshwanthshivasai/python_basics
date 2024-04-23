5e121e549f3e16d47f765ed2e0d238136f16457a
jeshwanthshivasai <havingthesensesofaspider@gmail.com>
2024-04-23 17:38:30 +0530

    change

--- kondachiluva.py
+++ kondachiluva.py
@@ -1144,4 +1144,19 @@ It's a special value meant to indicate the absence of a value)'''
 # used_car.increment_odometer(100)
 # used_car.read_odometer()
 #----------------------------------------------------------------------------------------------------------------
-'''Inheritance'''
+'''FROM FREE CODE CAMP - TUPLES (COMPARING AND SORTING TUPLES)'''
+# fhand=open('romeo.txt')
+# counts=dict()
+# for line in fhand:
+#     words=line.split()
+#     for word in words:
+#         counts[word]=counts.get(word, 0) + 1
+# lst=list()
+# for key, val in counts.items():
+#     newtup=(val, key)
+#     lst.append(newtup)
+# lst=sorted(lst, reverse=True)
+# for val, key in lst[:10]:
+#     print(key, val)
+'''FROM FREE CODE CAMP - TUPLES (COMPARING AND SORTING TUPLES)'''
+#----------------------------------------------------------------------------------------------------------------
